5a05e9c48405fb2c34e382ca2008ea7a8b5c3caa
Jefferson Lima <jeeffsantoos@outlook.com>
2025-05-07 16:49:25 -0300

    - feat/routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express';
 import productsRouter from '@modules/products/infra/http/routes/products.routes';
+import stockRouter from '@modules/stock/infra/http/routes/stockMovement.routes';
+import salesRouter from '@modules/sales/infra/http/routes/sales.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/stocks', stockRouter);
+routes.use('/sales', salesRouter);
 
 routes.get('/status', (request: Request, response: Response) => {
   return response.status(200).json({
