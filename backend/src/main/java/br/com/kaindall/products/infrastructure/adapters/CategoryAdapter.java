21275fc90bf8b46bd0f20837fdba2b47771fdbaf
Kaindall <gustavozanete2@gmail.com>
2025-05-10 05:31:24 -0300

    FEAT: assimilar a implementação do CategoryGateway

--- backend/src/main/java/br/com/kaindall/products/infrastructure/adapters/CategoryAdapter.java
+++ backend/src/main/java/br/com/kaindall/products/infrastructure/adapters/CategoryAdapter.java
@@ -1,9 +1,21 @@
 package br.com.kaindall.products.infrastructure.adapters;
 
 import br.com.kaindall.products.domain.gateways.CategoryGateway;
+import br.com.kaindall.products.domain.models.Category;
 
 public class CategoryAdapter implements CategoryGateway {
+    @Override
     public boolean save(String name, String description) {
+        return false;
+    }
+
+    @Override
+    public Category find(Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long categoryId) {
         return false;
     }
 }
